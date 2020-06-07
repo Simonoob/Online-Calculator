c7ad358572da85575bfeb1db8fcdb3f46b60a12c
Simone Fiorentini <simonefiorentini1@gmail.com>
2020-06-07 18:21:08 +0300

    started to implement aiddition calculation function

--- scipt.js
+++ scipt.js
@@ -1,23 +1,45 @@
 const numbers = document.querySelectorAll("button");
 const entry = document.querySelector(".display-text");
-let entryValue="";
+let currentEntry="";
+let lastEntry=0;
+let operator;
 
 
 numbers.forEach(number => number.addEventListener("click",message));
 function message(e){
     const targetId = e.target.id;
     if (e.target.classList.value === "number"){
-        entryValue += targetId;
+        currentEntry += targetId;
         entry.textContent += e.target.textContent;
-        console.log(entryValue);
     }
     else{
         console.log(targetId);
         switch(targetId){
             case "add":
-                entryValue+= "+";
+                operator = "+";
+                break;
+            case "subtract":
+                operator = "-";
                 break;
         }
-        
+        lastEntry = currentEntry;
+        console.log(operator);
+        calculate();
     } 
+}
+
+let result;
+
+function calculate(){
+    if (lastEntry!= 0 && currentEntry!= 0 && operator){
+        lastEntry = parseInt(lastEntry);
+        currentEntry = parseInt(currentEntry);
+        switch(operator){
+            case "+":
+                result = lastEntry + currentEntry;
+                break;
+        }
+        entry.textContent = result;
+    }
+    else{}
 }
